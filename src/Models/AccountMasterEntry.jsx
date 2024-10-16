09eb2f10f241346bcc843f7bbbdb5e507a173716
akhilss235 <146330955+akhilss235@users.noreply.github.com>
2024-10-16 16:54:47 +0530

    cooommitt

--- src/Models/AccountMasterEntry.jsx
+++ src/Models/AccountMasterEntry.jsx
@@ -5,13 +5,15 @@ import { Form, Row, Col, Button } from "react-bootstrap";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import request from "../Request"; // Adjust the path as necessary
 import useAccountHeads from "../hooks/useAccountHeads";
-import { toast } from 'react-toastify'; // Import toast
+import { toast } from "react-toastify"; // Import toast
+import Spinner from 'react-bootstrap/Spinner';
 
 function AccountMasterEntry({ open, onClose }) {
   const [accountHead, setAccountHead] = useState("");
   const [subAccountHead, setSubAccountHead] = useState("");
-  const { subAccountHeads , setSubAccountHeads } = useAccountHeads()
+  const { subAccountHeads, setSubAccountHeads } = useAccountHeads();
   const [errors, setErrors] = useState({ accountHead: "", subAccountHead: "" });
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,49 +22,64 @@ function AccountMasterEntry({ open, onClose }) {
 
     let isValid = true;
     if (!accountHead) {
-      setErrors((prev) => ({ ...prev, accountHead: "Account Head is required." }));
+      setErrors((prev) => ({
+        ...prev,
+        accountHead: "Account Head is required.",
+      }));
       isValid = false;
     }
     if (!subAccountHead) {
-      setErrors((prev) => ({ ...prev, subAccountHead: "Sub Account Head is required." }));
+      setErrors((prev) => ({
+        ...prev,
+        subAccountHead: "Sub Account Head is required.",
+      }));
       isValid = false;
     }
 
-    if (!isValid) return; 
+    if (!isValid) return;
 
     const data = {
       accountHead,
       subAccountHead,
     };
+    setLoading(true);
 
     try {
-      await request.post("addAccountMaster/", data); 
-      setAccountHead("")
-      setSubAccountHead("")
+      await request.post("addAccountMaster/", data);
+      setAccountHead("");
+      setSubAccountHead("");
       onClose();
-      toast.success("Account master added successfully"); 
-
+      toast.success("Account master added successfully");
     } catch (error) {
       console.error("Error:", error);
-      toast.error(error.response?.data?.message); 
-
+      toast.error(error.response?.data?.message);
+    } finally {
+      setLoading(false);
     }
   };
-
   const removeSubAccountHead = (indexToRemove) => {
-    setSubAccountHeads((prev)=>prev.filter((_,index)=> index !==indexToRemove ))
+    setSubAccountHeads((prev) =>
+      prev.filter((_, index) => index !== indexToRemove)
+    );
   };
   return (
     <Modal show={open} onHide={onClose} size="lg" centered>
       <Modal.Body>
         <div className="container-fluid p-3">
-          <Form className="roboto-font stylelabel styleinput" onSubmit={handleSubmit}>
+          <Form
+            className="roboto-font stylelabel styleinput"
+            onSubmit={handleSubmit}
+          >
             <Row className="justify-content-between align-items-center mt-2 mb-3">
               <Col xs={"auto"}>
                 <span className="modalformheading">New Account Head</span>
               </Col>
               <Col xs={"auto"}>
-                <IoIosCloseCircleOutline size={32} className="modalformclosebtn" onClick={onClose} />
+                <IoIosCloseCircleOutline
+                  size={32}
+                  className="modalformclosebtn"
+                  onClick={onClose}
+                />
               </Col>
             </Row>
 
@@ -70,7 +87,9 @@ function AccountMasterEntry({ open, onClose }) {
               <Col className="d-flex flex-column justify-content-between">
                 <Row>
                   <Col>
-                    <Form.Label column sm={12}>Account Head</Form.Label>
+                    <Form.Label column sm={12}>
+                      Account Head
+                    </Form.Label>
                   </Col>
                 </Row>
                 <Row>
@@ -82,8 +101,9 @@ function AccountMasterEntry({ open, onClose }) {
                       placeholder=""
                     />
                   </Col>
-                  {errors.accountHead && <div className="text-danger">{errors.accountHead}</div>}
-
+                  {errors.accountHead && (
+                    <div className="text-danger">{errors.accountHead}</div>
+                  )}
                 </Row>
               </Col>
             </Row>
@@ -92,7 +112,9 @@ function AccountMasterEntry({ open, onClose }) {
               <Col className="d-flex flex-column justify-content-between">
                 <Row>
                   <Col>
-                    <Form.Label column sm={12}>Sub Account Head</Form.Label>
+                    <Form.Label column sm={12}>
+                      Sub Account Head
+                    </Form.Label>
                   </Col>
                 </Row>
                 <Row>
@@ -103,54 +125,69 @@ function AccountMasterEntry({ open, onClose }) {
                       onChange={(e) => setSubAccountHead(e.target.value)}
                       placeholder=""
                     />
-                    {errors.subAccountHead && <div className="text-danger">{errors.subAccountHead}</div>}
+                    {errors.subAccountHead && (
+                      <div className="text-danger">{errors.subAccountHead}</div>
+                    )}
                   </Col>
-
                 </Row>
               </Col>
             </Row>
             <Row>
-              <Col className='py-2'>
-                  {subAccountHeads.map((subAccount, index) => (
-                    <div className='mt-3 me-3' key={index} style={{ display: "inline-block"}}>
-                      <button
-                        type="button"
+              <Col className="py-2">
+                {subAccountHeads.map((subAccount, index) => (
+                  <div
+                    className="mt-3 me-3"
+                    key={index}
+                    style={{ display: "inline-block" }}
+                  >
+                    <button
+                      type="button"
+                      style={{
+                        display: "inline-flex",
+                        alignItems: "center",
+                        padding: "5px",
+                        backgroundColor: "#ECF3FF",
+                        color: "#3474EB",
+                        border: "none",
+                        borderRadius: "5px",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {subAccount}
+                      <IoIosCloseCircleOutline
+                        size={18}
                         style={{
-                          display: "inline-flex",
-                          alignItems: "center",
-                          padding: "5px",
-                          backgroundColor: "#ECF3FF",
-                          color:'#3474EB',
-                          border: "none",
-                          borderRadius: "5px",
+                          marginLeft: "5px",
+                          color: "red",
                           cursor: "pointer",
                         }}
-                      >
-                        {subAccount}
-                        <IoIosCloseCircleOutline
-                        size={18}
-                          style={{
-                            marginLeft: "5px",
-                            color: "red",
-                            cursor: "pointer",
-                          }}
-                          onClick={() => removeSubAccountHead(index)}
-                        />
-                      </button>
-                    </div>
-                  ))}
-              
+                        onClick={() => removeSubAccountHead(index)}
+                      />
+                    </button>
+                  </div>
+                ))}
               </Col>
             </Row>
             <Row className="justify-content-end align-items-center my-4 gy-2">
               <Col xs={"auto"}>
-                <Button className="fw-600 modalformdiscardbtn" onClick={onClose}>
+                <Button
+                  className="fw-600 modalformdiscardbtn"
+                  onClick={onClose}
+                >
                   Discard
                 </Button>
               </Col>
               <Col xs={"auto"}>
-                <Button type="submit" className="fw-600 modalformsavebtn">
-                  Save
+                <Button
+                  type="submit"
+                  className="fw-600 modalformsavebtn"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <Spinner animation="border" variant="primary" />
+                  ) : (
+                    "Save"
+                  )}
                 </Button>
               </Col>
             </Row>
--- src/Models/AccountMasterUpdate.jsx
+++ src/Models/AccountMasterUpdate.jsx
@@ -3,161 +3,186 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Modal, Form, Row, Col, Button } from "react-bootstrap";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import request from "../Request"; // Adjust the path as necessary
-import { toast } from 'react-toastify'; // Import toast
+import { toast } from "react-toastify"; // Import toast
+import Spinner from "react-bootstrap/Spinner";
 
 function AccountMasterUpdate({ open, onClose, accountId, refreshData }) {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [formData, setFormData] = useState({
-        accountHead: '',
-        subAccountHead: ''
-    });
-    const [formErrors, setFormErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    accountHead: "",
+    subAccountHead: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
 
-    useEffect(() => {
-        const fetchAccountData = async () => {
-            if (accountId) {
-                try {
-                    setLoading(true);
-                    setError(null);
-                    const response = await request.get(`getAccountMasterById/${accountId}`);
-                    if (response.data && response.data.data) {
-                        const accountData = response.data.data;
-                        setFormData({
-                            accountHead: accountData.accountHead || '',
-                            subAccountHead: accountData.subAccountHead || ''
-                        });
-                    } else {
-                        setError("No account data found.");
-                    }
-                } catch (err) {
-                    setError("Error fetching account data. Please try again later.");
-                    console.error("Error fetching account data:", err);
-                } finally {
-                    setLoading(false);
-                }
-            } else {
-                setError("No account ID provided");
-            }
-        };
-
-        fetchAccountData();
-    }, [accountId]);
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-        setFormErrors((prevErrors) => ({
-            ...prevErrors,
-            [name]: value ? '' : 'This field is required'
-        }));
-    };
-
-    const validateForm = () => {
-        const errors = {};
-        if (!formData.accountHead) errors.accountHead = 'Account Head is required';
-        if (!formData.subAccountHead) errors.subAccountHead = 'Sub Account Head is required';
-        setFormErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        setError(null);
-
-        if (!validateForm()) {
-            setLoading(false);
-            return;
-        }
-
+  useEffect(() => {
+    const fetchAccountData = async () => {
+      if (accountId) {
         try {
-            const response = await request.put(`updateAccountMaster/${accountId}`, formData);
-            if (response.status === 200) {
-                refreshData(); // Refresh the table data
-                onClose(); // Close the modal after successful save
-                toast.success("Account master updated successfully"); 
-
-            } else {
-                setError("Error saving data. Please try again.");
-            }
+          setLoading(true);
+          setError(null);
+          const response = await request.get(
+            `getAccountMasterById/${accountId}`
+          );
+          if (response.data && response.data.data) {
+            const accountData = response.data.data;
+            setFormData({
+              accountHead: accountData.accountHead || "",
+              subAccountHead: accountData.subAccountHead || "",
+            });
+          } else {
+            setError("No account data found.");
+          }
         } catch (err) {
-            setError("Error saving data. Please try again later.");
-            toast.error(err.response?.data?.message); 
-
-            console.error("Error saving account data:", err);
+          setError("Error fetching account data. Please try again later.");
+          console.error("Error fetching account data:", err);
         } finally {
-            setLoading(false);
+          setLoading(false);
         }
+      } else {
+        setError("No account ID provided");
+      }
     };
 
-    return (
-        <Modal show={open} onHide={onClose} size="lg" centered>
-            <Modal.Body>
-                <div className="container-fluid p-3">
-                    {error && <div className="alert alert-danger">{error}</div>}
-                    <Form onSubmit={handleSubmit}>
-                        <Row className="justify-content-between align-items-center mt-2 mb-3">
-                            <Col xs={"auto"}>
-                                <span className="modalformheading">Edit Account Head</span>
-                            </Col>
-                            <Col xs={"auto"}>
-                                <IoIosCloseCircleOutline size={32} className="modalformclosebtn" onClick={onClose} />
-                            </Col>
-                        </Row>
+    fetchAccountData();
+  }, [accountId]);
 
-                        <Row>
-                            <Col className="d-flex flex-column justify-content-between">
-                                <Form.Label>Account Head</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="accountHead"
-                                    value={formData.accountHead}
-                                    onChange={handleInputChange}
-                                    placeholder="Enter Account Head"
-                                    required
-                                    isInvalid={!!formErrors.accountHead}
-                                />
-                                <Form.Control.Feedback type="invalid">{formErrors.accountHead}</Form.Control.Feedback>
-                            </Col>
-                        </Row>
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: value ? "" : "This field is required",
+    }));
+  };
 
-                        <Row className="mt-3">
-                            <Col className="d-flex flex-column justify-content-between">
-                                <Form.Label>Sub Account Head</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    name="subAccountHead"
-                                    value={formData.subAccountHead}
-                                    onChange={handleInputChange}
-                                    placeholder="Enter Sub Account Head"
-                                    required
-                                    isInvalid={!!formErrors.subAccountHead}
-                                />
-                                <Form.Control.Feedback type="invalid">{formErrors.subAccountHead}</Form.Control.Feedback>
-                            </Col>
-                        </Row>
+  const validateForm = () => {
+    const errors = {};
+    if (!formData.accountHead) errors.accountHead = "Account Head is required";
+    if (!formData.subAccountHead)
+      errors.subAccountHead = "Sub Account Head is required";
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
-                        <Row className="justify-content-end align-items-center my-4 gy-2">
-                            <Col xs={"auto"}>
-                                <Button className="fw-600 modalformdiscardbtn" onClick={onClose}>
-                                    Discard
-                                </Button>
-                            </Col>
-                            <Col xs={"auto"}>
-                                <Button type="submit" className="fw-600 modalformsavebtn" disabled={loading}>
-                                    {loading ? 'Saving...' : 'Save'}
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    if (!validateForm()) {
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await request.put(
+        `updateAccountMaster/${accountId}`,
+        formData
+      );
+      if (response.status === 200) {
+        refreshData(); // Refresh the table data
+        onClose(); // Close the modal after successful save
+        toast.success("Account master updated successfully");
+      } else {
+        setError("Error saving data. Please try again.");
+      }
+    } catch (err) {
+      setError("Error saving data. Please try again later.");
+      toast.error(err.response?.data?.message);
+
+      console.error("Error saving account data:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Modal show={open} onHide={onClose} size="lg" centered>
+      <Modal.Body>
+        <div className="container-fluid p-3">
+          {error && <div className="alert alert-danger">{error}</div>}
+          <Form onSubmit={handleSubmit}>
+            <Row className="justify-content-between align-items-center mt-2 mb-3">
+              <Col xs={"auto"}>
+                <span className="modalformheading">Edit Account Head</span>
+              </Col>
+              <Col xs={"auto"}>
+                <IoIosCloseCircleOutline
+                  size={32}
+                  className="modalformclosebtn"
+                  onClick={onClose}
+                />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="d-flex flex-column justify-content-between">
+                <Form.Label>Account Head</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="accountHead"
+                  value={formData.accountHead}
+                  onChange={handleInputChange}
+                  placeholder="Enter Account Head"
+                  required
+                  isInvalid={!!formErrors.accountHead}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {formErrors.accountHead}
+                </Form.Control.Feedback>
+              </Col>
+            </Row>
+
+            <Row className="mt-3">
+              <Col className="d-flex flex-column justify-content-between">
+                <Form.Label>Sub Account Head</Form.Label>
+                <Form.Control
+                  type="text"
+                  name="subAccountHead"
+                  value={formData.subAccountHead}
+                  onChange={handleInputChange}
+                  placeholder="Enter Sub Account Head"
+                  required
+                  isInvalid={!!formErrors.subAccountHead}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {formErrors.subAccountHead}
+                </Form.Control.Feedback>
+              </Col>
+            </Row>
+
+            <Row className="justify-content-end align-items-center my-4 gy-2">
+              <Col xs={"auto"}>
+                <Button
+                  className="fw-600 modalformdiscardbtn"
+                  onClick={onClose}
+                >
+                  Discard
+                </Button>
+              </Col>
+              <Col xs={"auto"}>
+                <Button
+                  type="submit"
+                  className="fw-600 modalformsavebtn"
+                  disabled={loading}
+                >
+                  {loading ? (
+                    <Spinner animation="border" variant="primary" />
+                  ) : (
+                    "Save"
+                  )}
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default AccountMasterUpdate;
